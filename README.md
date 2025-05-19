971d3e732c4419ff2e8101d8cc9f09ef9fe026fd
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-19 15:32:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# musicPlayerDarkTune
+
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="p-6 font-bold text-blue-500 text-center text 2xl">
+        ¡Proyecto de React + Vite + Tailwind + Typescript!
+      </div>
+    </>
+  );
+}
+
+export default App;
