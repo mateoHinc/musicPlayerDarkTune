acb1f66c045c12d42e2c084b4fd27aa059c29edc
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-20 14:33:28 -0500

    Add music.ts

---
+++ src/types/music.ts
@@ -0,0 +1,36 @@
+export interface Track {
+  id: string;
+  title: string;
+  artist: string;
+  album: string;
+  duration: number; // En segundo //In seconds
+  coverUrl: string;
+  audioUrl: string;
+}
+
+export type RepeatMode = "off" | "all" | "one";
+
+export interface PlayerState {
+  tracks: Track[];
+  currentTrackIndex: number;
+  isPlaying: boolean;
+  progress: number;
+  volume: number;
+  inMuted: boolean;
+  repeatMode: RepeatMode;
+  isShuffled: boolean;
+}
+
+export type PlayerAction =
+  | { type: "SET_TRACKS"; payload: Track[] }
+  | { type: "PLAY" }
+  | { type: "PAUSE" }
+  | { type: "TOGGLE_PLAY" }
+  | { type: "NEXT_TRACK" }
+  | { type: "PREV_TRACK" }
+  | { type: "SET_TRACK_INDEX"; payload: number }
+  | { type: "SET_PROGRESS"; payload: number }
+  | { type: "SET_VOLUME"; payload: number }
+  | { type: "TOGGLE_MUTE" }
+  | { type: "TOGGLE_SHUFFLE" }
+  | { type: "SET_REPEAT_MODE"; payload: RepeatMode };
