156aaea1801abffba5e4cb5f7d7e102e988f656a
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-05-30 11:50:51 -0500

    Add component AudioPlayer.tsx

---
+++ src/components/AudioPlayer.tsx
@@ -0,0 +1,15 @@
+import TrackList from "./TrackList";
+
+const AudioPlayer: React.FC = () => {
+  return (
+    <div className="flex flex-col bg-gradient-to-b from-gray-900 to-black shadow-2xl border border-gray-800 rounded-lg h-full overflow-hidden text-white">
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <TrackList />
+      </div>
+
+      <div className="bg-black bg-opacity-40 backdrop-blur-md border-gray-800 border-t"></div>
+    </div>
+  );
+};
+
+export default AudioPlayer;
