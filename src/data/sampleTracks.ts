d6f0139949ec9280f7dd3feb85cd85a247b09789
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-06-07 14:53:48 -0500

    Add tracks to playlist

--- src/data/sampleTracks.ts
+++ src/data/sampleTracks.ts
@@ -3,57 +3,131 @@ import type { Track } from "../types/music";
 export const sampleTracks: Track[] = [
   {
     id: "1",
-    title: "Sunset Memories",
-    artist: "Ambient Dreams",
-    album: "Coastal Views",
-    duration: 214,
+    title: "Rip & Tear",
+    artist: "Mick Gordon",
+    album: "Doom (Original Game Soundtrack)",
+    duration: 254,
     coverUrl:
-      "https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "https://i1.sndcdn.com/artworks-v7mY1j1Nz5zRZZ9w-kT1kNg-t500x500.jpg",
     audioUrl:
       "https://cdn.pixabay.com/download/audio/2022/02/07/audio_4b1c3215fe.mp3?filename=lofi-study-112191.mp3",
   },
   {
     id: "2",
-    title: "Ocean Waves",
-    artist: "Nature Sounds",
-    album: "Peaceful Escape",
-    duration: 187,
+    title: "Amsterdam",
+    artist: "Nothing But Thieves",
+    album: "Broken Machine (Deluxe)",
+    duration: 272,
     coverUrl:
-      "https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "https://lamusikalite.com/wp-content/uploads/2017/10/c8a0dba90778be71f3da065544473e13-1000x1000x1.jpg",
     audioUrl:
       "https://cdn.pixabay.com/download/audio/2022/05/27/audio_246b51d686.mp3?filename=lofi-chill-medium-version-159456.mp3",
   },
   {
     id: "3",
-    title: "Urban Night",
-    artist: "City Pulse",
-    album: "Downtown Beats",
-    duration: 243,
-    coverUrl:
-      "https://images.pexels.com/photos/2078071/pexels-photo-2078071.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Roll the windows up",
+    artist: "MGK",
+    album: "Lockdown sessions",
+    duration: 204,
+    coverUrl: "https://i1.sndcdn.com/artworks-MTflfrXqQGRa-0-t500x500.jpg",
     audioUrl:
       "https://cdn.pixabay.com/download/audio/2022/10/25/audio_946550d903.mp3?filename=lo-fi-chill-hip-hop-song-158800.mp3",
   },
   {
     id: "4",
-    title: "Mountain High",
-    artist: "Peak Climbers",
-    album: "Above Clouds",
-    duration: 198,
-    coverUrl:
-      "https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Let It Happen",
+    artist: "Sweeps",
+    album: "From past to future",
+    duration: 135,
+    coverUrl: "https://f4.bcbits.com/img/a3892822597_16.jpg",
     audioUrl:
       "https://cdn.pixabay.com/download/audio/2022/01/18/audio_f52a5165b7.mp3?filename=chilled-acoustic-indie-folk-instrumental-background-music-for-videos-5720.mp3",
   },
   {
     id: "5",
-    title: "Rainy Day",
-    artist: "Weather Sounds",
-    album: "Seasonal Moods",
-    duration: 221,
-    coverUrl:
-      "https://images.pexels.com/photos/1915182/pexels-photo-1915182.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Hold It Together",
+    artist: "Mike Shinoda",
+    album: "Post Traumatic (Deluxe version)",
+    duration: 205,
+    coverUrl: "https://i1.sndcdn.com/artworks-3krCJd2cUFGt-0-t500x500.jpg",
     audioUrl:
       "https://cdn.pixabay.com/download/audio/2021/11/23/audio_cb2a1fa976.mp3?filename=inspiring-cinematic-ambient-116199.mp3",
+  },
+  {
+    id: "6",
+    title: "Neurotica",
+    artist: "Polyphia",
+    album: "Remember That You Will Die",
+    duration: 195,
+    coverUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr_02isKykH7VVGrmjiiCD4w7VEZ5GCh2SnQ&s",
+    audioUrl:
+      "https://drive.google.com/file/d/1u-rNnnXYufdw7Q19sHeRGaE45kIgnmxt/view",
+  },
+  {
+    id: "7",
+    title: "Royal Leash",
+    artist: "Processor",
+    album: "Majesty",
+    duration: 223,
+    coverUrl:
+      "https://i.scdn.co/image/ab67616d0000b273763ba3a1e49deeab2daf7aac",
+    audioUrl:
+      "https://drive.google.com/file/d/1u-rNnnXYufdw7Q19sHeRGaE45kIgnmxt/view",
+  },
+  {
+    id: "8",
+    title: "Push It",
+    artist: "Static X",
+    album: "Wisconsin Death Trip",
+    duration: 154,
+    coverUrl:
+      "https://i.discogs.com/SSdnpQ6nUZiLAJSHEolISgwVsYZVeF7k4tO1QkeIq-Y/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEzNjAz/Nzc4LTE1NTc0OTUz/NzUtODk3NS5qcGVn.jpeg",
+    audioUrl:
+      "https://drive.google.com/file/d/1u-rNnnXYufdw7Q19sHeRGaE45kIgnmxt/view",
+  },
+  {
+    id: "9",
+    title: "Stranded",
+    artist: "Gojira",
+    album: "Magma",
+    duration: 269,
+    coverUrl:
+      "https://cdn-images.dzcdn.net/images/cover/cc9b867b6847741d0b64b79f85982f1d/1900x1900-000000-80-0-0.jpg",
+    audioUrl:
+      "https://drive.google.com/file/d/1u-rNnnXYufdw7Q19sHeRGaE45kIgnmxt/view",
+  },
+  {
+    id: "10",
+    title: "Knee Socks",
+    artist: "Arctic Monkeys",
+    album: "AM",
+    duration: 257,
+    coverUrl:
+      "https://i.scdn.co/image/ab67616d0000b2734ae1c4c5c45aabe565499163",
+    audioUrl:
+      "https://drive.google.com/file/d/1u-rNnnXYufdw7Q19sHeRGaE45kIgnmxt/view",
+  },
+  {
+    id: "11",
+    title: "Body High",
+    artist: "Mystery Skulls",
+    album: "Forever",
+    duration: 200,
+    coverUrl:
+      "https://cdn-images.dzcdn.net/images/cover/54da8001158dbc08d8090dada1fb1f43/0x1900-000000-80-0-0.jpg",
+    audioUrl:
+      "https://drive.google.com/file/d/1u-rNnnXYufdw7Q19sHeRGaE45kIgnmxt/view",
+  },
+  {
+    id: "12",
+    title: "Renegades",
+    artist: "X Ambassadors",
+    album: "VHS",
+    duration: 181,
+    coverUrl:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/d/d8/VHS_X_ambassadors.jpg/250px-VHS_X_ambassadors.jpg",
+    audioUrl:
+      "https://drive.google.com/file/d/1u-rNnnXYufdw7Q19sHeRGaE45kIgnmxt/view",
   },
 ];
